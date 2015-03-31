bcad2e49d998c895c66e37497359026a9ef6190e
andy.lv <andy.lv@live.com>
2015-03-31 23:59:36 +0800

    add toolkit-pen

---
+++ src/module/toolkit/pen.js
@@ -0,0 +1,17 @@
+/**
+ * @author: Andy.Lv
+ * @description: 钢笔工具
+ *
+ */
+;(function (Ps) {
+  'use strict';
+
+  window[Ps].module("toolkit-pen", function (P) {
+    return {
+      draw: function () {
+        //window[Ps](this.canvas.width, this.canvas.height, "#000").act("喷点").act("反色").act("浮雕效果")
+      }
+    };
+  });
+
+})("psLib");
